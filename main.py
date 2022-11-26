5d4ec8c6893cf66509872d180d806039db0509a9
azozrazoz <doshan.de@gmail.com>
2022-11-26 12:08:55 +0600

    some fixes

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def test(interaction):
 
 
 @bot.command(name="kurs")
-async def keep_alive(ctx):
+async def kurs(ctx):
     data = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
 
     await ctx.send(embed=nextcord.Embed(title="Курс на сегодня:", color=0xffffff, description=data['Date'])
@@ -40,44 +40,4 @@ async def on_ready():
     print("BOT ONLINE!")
 
 
-@bot.event  
-async def on_member_join(member: nextcord.member.Member):
-    channel = bot.get_channel(main_data[0]['join'])
-
-    await channel.send(embed=nextcord.Embed(title=f"{member.name} итак, здравствуй!", color=0xffffff))
-
-
-@bot.listen('on_message')
-async def on_message(ctx):
-    if ctx.channel.id == 1035189687427534848:
-        member = ctx.author
-        role = nextcord.utils.get(member.guild.roles, id=main_data[0]['id_role_new_member'])
-
-        await member.add_roles(role)
-
-
-@bot.event
-async def on_raw_reaction_add(payload):
-    message_id = payload.message_id
-    
-    if message_id == 1041687082713763890: 
-        for role_name in main_data[1]:
-            if payload.emoji.name == role_name:
-                role = nextcord.utils.get(payload.member.guild.roles, id=main_data[1][role_name])
-                await payload.member.add_roles(role)
-                break
-
-
-@bot.event
-async def on_raw_reaction_remove(payload):
-    messageid = payload.message_id
-    if messageid == 1041687082713763890: 
-        for role_name in main_data[1]:
-            if payload.emoji.name == role_name: 
-                member = nextcord.utils.get(bot.get_all_members(), id=payload.user_id)
-                role = nextcord.utils.get(member.guild.roles, id=main_data[1][role_name])
-                await member.remove_roles(role)
-                break
-
-
-bot.run("token")
+bot.run("MTA0MTYxNDI0MTM2ODY0OTc3OA.GV4rYH.GUmyRENl6mzvuh1OBwEXami5oDEazGlYu4C_8Y")
